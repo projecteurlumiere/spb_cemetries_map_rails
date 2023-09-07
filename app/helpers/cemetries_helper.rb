f0ce87eed0257e9f2e762beef6481ee53d32d7dc
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-09-07 13:30:51 +0300

    initial commit

---
+++ app/helpers/cemetries_helper.rb
@@ -0,0 +1,2 @@
+module CemetriesHelper
+end
---
+++ test/controllers/cemetries_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CemetriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
