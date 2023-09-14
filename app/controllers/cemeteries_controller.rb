a29257aea5db524c609aad118d8c2f03e586ad31
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-09-14 11:17:03 +0300

    show action now renders entire page instead of partial

--- app/controllers/cemeteries_controller.rb
+++ app/controllers/cemeteries_controller.rb
@@ -1,10 +1,8 @@
 class CemeteriesController < ApplicationController
   def index
-    @cemeteries = Cemetery.all
   end
   
   def show
     @cemetery = Cemetery.find_by(id: params[:id])
-    render partial: 'show'
   end
 end
