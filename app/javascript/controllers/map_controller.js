b2ca75b3c5ceb8972e6a5164fb705094910969d7
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-09-17 16:05:42 +0300

    tidy up stimulus  map controller

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,23 +3,15 @@ import L from 'leaflet'
 import "leaflet-sidebar-v2"
 
 export default class extends Controller {
-  // static values = {
-  //   coordinates: String,
-  // }
+  cityCoordinates = [59.8965, 30.3264]; // coordinates for spb city center
+  polygons = {};
   
   connect() {
-    this.polygons = {}
-    this.map = L.map('map').setView([59.8965, 30.3264], 10);
+    this.#isShowRequest();  // if true, it will zoom map to the desired entry
+    
+    this.map = L.map('map').setView(this.cityCoordinates, 10);
 
-    if (window.location.toString().includes("/cemeteries/")) {
-      this.isShowRequest = true
-      this.initialEntryId = document.getElementById('entry').getAttribute('data-cemetery-id-value')
-    }
-    else this.isShowRequest = false
-
-    console.log(this.isShowRequest)
-
-    L.tileLayer(`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}?api_key=1`, {
+    L.tileLayer(`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}`, {
       minZoom: 10,
       attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       ext: 'png'
@@ -32,8 +24,6 @@ export default class extends Controller {
     }).addTo(this.map);
     
     this.sidebar.open('list-tab');
-
-    console.log('map controller has done its work')
   }
 
   drawPolygon(e) {
@@ -44,36 +34,40 @@ export default class extends Controller {
       polygon.addTo(this.map);
 
       polygon.addEventListener('click', () => {
-        this.centerMap(polygon);
-        this.sidebar.open('entry-info')
+        this.#toEntry(polygon);
         Turbo.visit(`/cemeteries/${e.detail.id}`, { action: 'advance', frame: 'entry' }); // doesn't update URL despite action: advance- apparently, rails bug
       })
-
-      if (this.isShowRequest === true && this.initialEntryId == e.detail.id) {
-        this.centerMap(polygon)
-        this.sidebar.open('entry-info')
-      }
-
-      console.log('I have drawn a polygon!')
     }
   }
 
-  toEntry(e) {
-    console.log(typeof e)
-    console.log(e.target);
-    console.log(e.target.getAttribute("data-cemetery-id-value"));
+  // in the app view, drawPolygon fires first followed by this method:
+  toInitialEntry(e) { 
+    if (this.isShowRequest === true && this.initialEntryId == e.detail.id) {
+      this.toEntryByEvent(e);
+    }
+  } 
 
+  toEntryByEvent(e) {
     let polygon = this.polygons[e.target.getAttribute("data-cemetery-id-value")];
-    
-    this.centerMap(polygon);
+    this.#toEntry(polygon);
+  }
 
+  #toEntry(polygon){
+    this.#centerMap(polygon);
     this.sidebar.open('entry-info');
   }
 
-  centerMap(polygon) {
+  #centerMap(polygon) {
     if (polygon) {
       this.map.flyTo(polygon.getCenter(), 14)
     }
   }
 
+  #isShowRequest() {
+    if (window.location.toString().includes("/cemeteries/")) {
+      this.isShowRequest = true
+      this.initialEntryId = document.getElementById('entry').getAttribute('data-cemetery-id-value')
+    }
+    else this.isShowRequest = false
+  }
 }
